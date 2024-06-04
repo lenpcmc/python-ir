3674b1574017d263d7d80282776bb3104e9eff64
Lennon <lenpcmc@gmail.com>
2024-06-04 11:48:26 -0400

    reference graph

--- phonons.py
+++ phonons.py
@@ -17,17 +17,19 @@ im_root = "./resources/images/"
 arr_root = "./resources/arrays/"
 cif_root = "./resources/cifs"
 
+dConv = 15.63
+
+
 def main():
     for file in os.listdir(f"{cif_root}"):
-        fig, ax = plt.subplots()
         fig, ax = vdosPlot(fname = f"{file}", numAtoms = 1000, width = 100)
         plt.savefig(f"{im_root}/{file[:-4]}.png", dpi = 500)
     return
 
 
-def vdosPlot(fname: str = "betaCristobalite.cif", numAtoms = 20, width = 20):
+def vdosPlot(Dyn: np.ndarray = np.load("resources/arrays/betaCristobalite.npy"), width: int = 50):
     #Init
-    c,d = vdos(fname = fname, numAtoms = numAtoms)
+    c,d = vdos(Dyn)
 
     x = np.linspace(0, np.max(d), width)
     y = np.histogram(d, bins = np.linspace(0, np.max(d), width + 1))[0]
@@ -44,29 +46,21 @@ def vdosPlot(fname: str = "betaCristobalite.cif", numAtoms = 20, width = 20):
     return fig, ax
 
 
-def vdos(fname: str, numAtoms: int = 20, exclude: bool = True, save: bool = True):
-    atoms = buildNumber(f"{cif_root}/{fname}", numAtoms)
-    Dyn = dynamical(atoms)
+def vdos(Dyn: np.ndarray, exclude: bool = True, save: str = False):
     D,C = np.linalg.eigh(Dyn)
     D = np.sqrt(D)
 
-    dConv = dynamicalConversion(1)
     c = C * dConv
     d = D * dConv
 
     if (exclude):
         d = np.nan_to_num(d)
 
-    if (save):
-        np.save(f"{arr_root}/{fname[:-4]}.npy", Dyn)
+    if bool(save):
+        np.save(save, Dyn)
 
     return c,d
 
-
-def dynamicalConversion(vals: float) -> float:
-    # 98.225 (eV / A^2 amu) per (J / kg m) / 2pi
-    # return vals / 15.63
-    return ( vals * (9.822517e13 / 1.e12) )/(6.28)
 
 
 if __name__ == "__main__":
