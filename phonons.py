2bcc27118e5da0098cddf4c46f8706ad2d2f6f57
Lennon <lenpcmc@gmail.com>
2024-06-06 12:28:22 -0400

    reference plots

--- phonons.py
+++ phonons.py
@@ -21,31 +21,36 @@ dConv = 15.63
 
 
 def main():
+    atoms = buildNumber(f"{cif_root}/betaCristobalite.cif", 1000)
+    #Dyn = dynamical(atoms)
+    #vdos(Dyn, save = f"{arr_root}/betaCristobalite.npy")
+
     for file in os.listdir(f"{cif_root}"):
         Dyn = np.load(f"{cif_root}/{file}")
-        fig,ax = plt.subplots()
-        ax = vdosPlot(ax, Dyn, width = 100)
-        plot.show()
+        vdosPlot(Dyn, width = 100, name = file[:-4])
+        #plot.show()
         #plt.savefig(f"{im_root}/{file[:-4]}.png", dpi = 500)
     return
 
 
-def vdosPlot(ax, Dyn: np.ndarray = np.load("resources/arrays/betaCristobalite.npy"), width: int = 50):
+def vdosPlot(Dyn: np.ndarray = np.load("resources/arrays/betaCristobalite.npy"), width: int = 50, name = ""):
     #Init
     c,d = vdos(Dyn)
 
     x = np.linspace(0, np.max(d), width)
     y = np.histogram(d, bins = np.linspace(0, np.max(d), width + 1))[0]
 
+    fig,ax = plt.subplots()
     ax.plot(x, y)
 
     ax.set_xlabel(r"$\nu$ [THz]")
     ax.xaxis.set_tick_params(which = "minor", bottom = False)
-
     ax.set_ylabel(r"VDOS [A.U.]")
     ax.set_yticklabels([])
     ax.yaxis.set_tick_params(which = "minor", bottom = False)
 
+    plt.savefig(f"{im_root}/{name}.png")
+    plt.show()
     return ax
 
 
